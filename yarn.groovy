18355479c23970d0db61e4eee75e47a3ce2d7455
Muhammad Aizuddin <aizuddin.zali@gmail.com>
2022-08-10 14:50:29 +0800

    add source

--- yarn.groovy
+++ yarn.groovy
@@ -89,7 +89,7 @@ pipeline {
                                 set +x
                                 pwd
                                 ls -lart ../
-                                sonar-scanner -X -Dsonar.verbose=true -Dsonar.login=7cf7aa1cc26a7f21354e5b89e255ba16d220e4e7 -Dsonar.projectKey=nodejs -Dsonar.host.url=http://sonarqube.cicd.svc.cluster.local:9000
+                                sonar-scanner -X -Dsonar.sources=. -Dsonar.verbose=true -Dsonar.login=7cf7aa1cc26a7f21354e5b89e255ba16d220e4e7 -Dsonar.projectKey=nodejs -Dsonar.host.url=http://sonarqube.cicd.svc.cluster.local:9000
 
                                 export PATH=~/bin:$PATH
                                 echo yarnrc content
